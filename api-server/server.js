b9af8f7c16c7fd2357ff2b8527c867855253803a
Nidhi1314 <nidhishekhawat945@gmail.com>
2026-03-16 23:39:32 +0530

    set up redis and env

--- api-server/server.js
+++ api-server/server.js
@@ -1,15 +1,35 @@
 import express from "express";
+import dotenv from "dotenv";
+import Redis from "ioredis";
+import crypto from "crypto";
+dotenv.config();
 const app=express();
 const port=3000;
+console.log("My Redis URL is:", process.env.REDIS_URL);
+const redis=new Redis(process.env.REDIS_URL);
 app.use(express.json());
 app.get('/ping',(req,res)=>{
    res.status(200).json({status:"api is running"});
 });
 //main route
-app.post('/submit',(req,res)=>{
+app.post('/submit',async (req,res)=>{
     const {language,code}=req.body;
     console.log(language,code);
-    res.status(200).json({status:"code received by api"});
+    const jobId=crypto.randomUUID();
+    const jobData={
+        jobId,language,code
+    };
+    try{
+        await redis.lpush('code_queue',JSON.stringify(jobData));
+        console.log(`job ${jobId} added to queue`);
+        res.status(200).json({
+            message:"code sbmitted sucessfully",
+            jobId:jobId
+        });
+    }catch(error){
+        console.log("redis error",error);
+        res.status(500).json({error:"failed to add job to queue"});
+    }
 })
 app.listen(port,()=>{
     console.log('server is running on http://localhost:${port}')
